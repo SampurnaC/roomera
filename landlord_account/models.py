9b3862183ab17dd35ecc121d8b6a7b2bc0b83908
sam <sampurna.chapagainnn@gmail.com>
2025-01-02 08:26:47 +0000

    add CustomUser and LandlordProfile models

--- landlord_account/models.py
+++ landlord_account/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    is_landlord = models.BooleanField(default=False)
+    phone=models.CharField(max_length=15, blank=True, null=True)
+    GENDER_CHOICES=[
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ]
+    gender=models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
+    date_of_birth=models.DateField(blank=True, null=True),
+    address=models.TextField(blank=True, null=True),
+    
+    def __str__(self):
+        return self.username
+
+class LandlordProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name="landlord_profile")
+    properties_owned = models.TextField(blank=True, null=True)
+    business_name = models.CharField(max_length=100, blank=True, null=True)
+    is_verified = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return f"{self.user.username}'s Landlord Profile"
