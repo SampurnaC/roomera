a7554a45021fd9b06041e861b0480836e2e9686c
sam <sampurna.chapagainnn@gmail.com>
2025-01-09 19:54:28 +0000

    add Property and Room model

--- landlord_account/models.py
+++ landlord_account/models.py
@@ -10,8 +10,8 @@ class CustomUser(AbstractUser):
         ('O', 'Other'),
     ]
     gender=models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True, null=True)
-    date_of_birth=models.DateField(blank=True, null=True),
-    address=models.TextField(blank=True, null=True),
+    date_of_birth=models.DateField(blank=True, null=True)
+    address=models.TextField(blank=True, null=True)
     
     def __str__(self):
         return self.username
@@ -24,3 +24,28 @@ class LandlordProfile(models.Model):
     
     def __str__(self):
         return f"{self.user.username}'s Landlord Profile"
+
+class Property(models.Model):
+    landlord = models.ForeignKey(LandlordProfile, on_delete=models.CASCADE, related_name="properties")
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+    address=models.TextField()
+    city=models.CharField(max_length=100)
+    is_active=models.BooleanField(default=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+class Room(models.Model):
+    property=models.ForeignKey(Property, on_delete=models.CASCADE, related_name="rooms"),
+    title=models.CharField(max_length=100)
+    description=models.TextField()
+    rent_price=models.DecimalField(max_digits=10, decimal_places=2)
+    max_occupancy=models.IntegerField()
+    is_available=models.BooleanField(default=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"{self.title} - {self.property.name}"
+
