2439db480d57d2482b1a6362755fff286f0f8444
sam <sampurna.chapagainnn@gmail.com>
2025-01-12 21:54:44 +0000

    add login url for landlord

--- landlord_account/urls.py
+++ landlord_account/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import index, landlord_register, landlord_dashboard
+from .views import landlord_register,landlord_login,landlord_dashboard
 
 app_name="roomera"
 
 urlpatterns = [
-    path('', index, name='index'),
     path('register/', landlord_register, name='landlord_register'),
+    path('login/', landlord_login, name='landlord_login'),
     path('dashboard/', landlord_dashboard, name='landlord_dashboard'),
 ]
