404f00a53a047954407506307ca7ffe9d48a4220
sam <sampurna.chapagainnn@gmail.com>
2025-01-21 23:57:21 +0000

    remove property and room model from landlord account app

--- landlord_account/models.py
+++ landlord_account/models.py
@@ -25,27 +25,5 @@ class LandlordProfile(models.Model):
     def __str__(self):
         return f"{self.user.username}'s Landlord Profile"
 
-class Property(models.Model):
-    landlord = models.ForeignKey(LandlordProfile, on_delete=models.CASCADE, related_name="properties")
-    name=models.CharField(max_length=100)
-    description=models.TextField()
-    address=models.TextField()
-    city=models.CharField(max_length=100)
-    is_active=models.BooleanField(default=True)
-    created_at=models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return self.name
 
-class Room(models.Model):
-    property=models.ForeignKey(Property, on_delete=models.CASCADE, related_name="rooms"),
-    title=models.CharField(max_length=100)
-    description=models.TextField()
-    rent_price=models.DecimalField(max_digits=10, decimal_places=2)
-    max_occupancy=models.IntegerField()
-    is_available=models.BooleanField(default=True)
-    created_at=models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return f"{self.title} - {self.property.name}"
 
