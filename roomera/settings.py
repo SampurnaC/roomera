dc10120a40d83030d11c37a5639d23ff30cb764d
sam <sampurna.chapagainnn@gmail.com>
2025-01-22 23:58:26 +0000

    load bootstrap

--- roomera/settings.py
+++ roomera/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
-    'bootstrap_v5',
+    'bootstrap5',
     "crispy_forms",
     "crispy_bootstrap5",
     
