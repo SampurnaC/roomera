87daa32e776cdfb99d25d69f8c92c77aba9400a3
sam <sampurna.chapagainnn@gmail.com>
2025-01-29 23:58:49 +0000

    add url for room detail feature

--- landlord_property/urls.py
+++ landlord_property/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import index,add_property,add_room,search_rooms
+from .views import index,add_property,add_room,search_rooms, room_detail
 
 app_name="landlord_property"
 
 urlpatterns = [
     path('', index, name='home'),
     path('rooms/search/', search_rooms, name='search_rooms'),
+    path('rooms/<room_id>/', room_detail,name='room_detail'),
     path('property/add/', add_property, name='add_property'),
     path('property/<int:property_id>/add-room', add_room, name='add_room'),
 ]
