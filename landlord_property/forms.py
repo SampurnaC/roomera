c153b071e50ab3fc1247a9cd8ca82de3d7a92c9b
sam <sampurna.chapagainnn@gmail.com>
2025-02-06 00:36:14 +0000

    create a seprate room image forms

--- landlord_property/forms.py
+++ landlord_property/forms.py
@@ -1,25 +1,32 @@
 from django import forms
-from .models import Property,Room
+from .models import Property,Room,RoomImage
 
 class PropertyForm(forms.ModelForm):
-    class Meta:
-        model=Property
-        fields=['name','description','address','postcode','is_active']
+  class Meta:
+      model=Property
+      fields=['name','description','address','postcode','is_active']
         
 class RoomForm(forms.ModelForm):
-    class Meta:
-        model = Room
-        fields = ['title', 'description', 'rent_price', 'max_occupancy', 'is_available','image']
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Room Title'}),
-            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Room Description'}),
-            'rent_price': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Rent Price'}),
-            'max_occupancy': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max Occupancy'}),
-            'is_available': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
-        }
-        
+  class Meta:
+      model = Room
+      fields = ['title', 'description', 'rent_price', 'max_occupancy', 'is_available']
+      widgets = {
+          'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Room Title'}),
+          'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Room Description'}),
+          'rent_price': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Rent Price'}),
+          'max_occupancy': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Max Occupancy'}),
+          'is_available': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+      }
+
+class RoomImageForm(forms.ModelForm):
+  image=forms.ImageField(widget=forms.ClearableFileInput(attrs={'multipart': True}))
+  
+  class Meta:
+    model=RoomImage
+    fields=['image']
+  
 class RoomSearchForm(forms.Form):
-    postcode=forms.CharField(max_length=100,required=False,label="Post Code")
-    min_rent=forms.DecimalField(max_digits=10,decimal_places=2,required=False,label="Min Rent")
-    max_rent=forms.DecimalField(max_digits=10,decimal_places=2,required=False,label="Max Rent")
-    max_occupancy=forms.IntegerField(required=False,label="Max Occupancy")
+  postcode=forms.CharField(max_length=100,required=False,label="Post Code")
+  min_rent=forms.DecimalField(max_digits=10,decimal_places=2,required=False,label="Min Rent")
+  max_rent=forms.DecimalField(max_digits=10,decimal_places=2,required=False,label="Max Rent")
+  max_occupancy=forms.IntegerField(required=False,label="Max Occupancy")
