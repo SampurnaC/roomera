fadd6bcd9a97a1053ac1a5e4f727c1ef31b281dd
sam <sampurna.chapagainnn@gmail.com>
2025-02-08 17:47:40 +0000

    work on verify email function to check if the user is verified or not

--- landlord_account/views.py
+++ landlord_account/views.py
@@ -1,21 +1,53 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, get_user_model
 from django.contrib.auth.decorators import login_required
 from .forms import LandlordSignUpForm,LandlordSignInForm
+from .email import send_verification_email
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_decode
+from django.utils.encoding import force_str
+from .models import LandlordProfile
+from django.contrib import messages
+from .tokens import verify_verification_token
+
+User=get_user_model()
 
 def landlord_register(request):
-    if request.method=="POST":
-        form=LandlordSignUpForm(request.POST)
-        if form.is_valid():
-            user=form.save()
-            login(request, user)
-            return redirect('landlord_account:landlord_dashboard')
+  if request.method=="POST":
+    form=LandlordSignUpForm(request.POST)
+    if form.is_valid():
+        user=form.save(commit=False)
+        user.is_verified=False
+        user.save()
+        
+        LandlordProfile.objects.create(
+          user=user,
+          properties_owned=form.cleaned_data.get('properties_owned'),
+          business_name=form.cleaned_data.get('business_name')
+        )
+
+        send_verification_email(user, request)
+        return redirect('landlord_account:landlord_dashboard')
+  else:
+    form=LandlordSignUpForm()
+
+  return render(request, 'landlord_account/landlord_register.html', {'form': form})
+
+def verify_email(request, uidb64, token):
+    user = verify_verification_token(uidb64, token)
+    
+    if user:
+        landlord_profile = LandlordProfile.objects.get(user=user)
+        landlord_profile.is_verified = True
+        landlord_profile.save()
+        
+        # login(request, user)
+        messages.success(request, "Your email has been verified!")
+        return redirect("landlord_account:landlord_login")
     else:
-        form=LandlordSignUpForm()
-
-    return render(request, 'landlord_account/landlord_register.html', {'form': form})
-
-
+        messages.error(request, "The verification link is invalid or has expired.", extra_tags='danger')
+        return redirect("landlord_account:landlord_register")
+  
 def landlord_login(request):
     if request.method=="POST":
         form=LandlordSignInForm(data=request.POST)
